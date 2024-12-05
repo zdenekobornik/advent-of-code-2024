20635917c5660c9b66764ed687747e507799ed7d
Zdeněk Oborník <zdenek@obornik.cz>
2024-12-05 01:37:04 +0100

    Add solution for Day 3

---
+++ src/Day03.kt
@@ -0,0 +1,33 @@
+fun main() {
+    fun part1(input: String): Int {
+        return "mul\\((\\d+),(\\d+)\\)".toRegex()
+            .findAll(input)
+            .sumOf { it.groupValues[1].toInt() * it.groupValues[2].toInt() }
+    }
+
+    fun part2(input: String): Int {
+        var enabled = true
+        return "mul\\((\\d+),(\\d+)\\)|don\'t\\(\\)|do\\(\\)".toRegex()
+            .findAll(input)
+            .filter {
+                enabled = when (it.value) {
+                    "don't()" -> false
+                    "do()" -> true
+                    else -> return@filter enabled
+                }
+                return@filter false
+            }
+            .sumOf { it.groupValues[1].toInt() * it.groupValues[2].toInt() }
+    }
+
+    // Or read a large test input from the `src/Day01_test.txt` file:
+    val testInput = readInput("Day03_test").joinToString(separator = "")
+    val testInput2 = readInput("Day03_test2").joinToString(separator = "")
+    check(part1(testInput) == 161)
+    check(part2(testInput2) == 48)
+
+    // Read the input from the `src/Day01.txt` file.
+    val input = readInput("Day03").joinToString(separator = "")
+    part1(input).println()
+    part2(input).println()
+}
