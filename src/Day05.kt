d7370fd352624745a54ccd5b55d9db6920ab0910
Zdeněk Oborník <zdenek@obornik.cz>
2024-12-18 22:21:04 +0100

    Add Day 5 pt. 2

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,8 +1,13 @@
 fun main() {
-    fun part1(input: String): Int {
+    fun prepareData(input: String): Pair<List<Pair<String, String>>, List<List<String>>> {
         val (rulesStr, pagesStr) = input.split("\n\n")
         val rules = rulesStr.lines().map { it.split('|').let { it[0] to it[1] } }
         val updates = pagesStr.lines().map { it.split(',') }
+        return rules to updates
+    }
+
+    fun part1(input: String): Int {
+        val (rules, updates) = prepareData(input)
         return updates.sumOf { update ->
             val invalidPages = mutableSetOf<String>()
             for (page in update) {
@@ -14,14 +19,30 @@ fun main() {
     }
 
     fun part2(input: String): Int {
-        return -1
+        val (rules, updates) = prepareData(input)
+        return updates
+            .map { it.toMutableList() }
+            .sumOf { update ->
+                var swapped = false
+                for (i in update.indices) {
+                    for (j in i + 1 until update.size) {
+                        if ((update[j] to update[i]) in rules) {
+                            val temp = update[i]
+                            update[i] = update[j]
+                            update[j] = temp
+                            swapped = true
+                        }
+                    }
+                }
+                if (swapped) update[update.size / 2].toInt() else 0
+            }
     }
 
     val testInput = readRawInput("Day05_test")
     check(part1(testInput) == 143)
-    //check(part2(testInput) == -1)
+    check(part2(testInput) == 123)
 
     val input = readRawInput("Day05")
     part1(input).println()
-    //part2(input).println()
+    part2(input).println()
 }
